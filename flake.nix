3e6698bd5dc036defaafb9874fa0e3c59599233e
Olaf Targowski <me@otsrv.net>
2025-10-10 20:22:52 +0200

    Update sioworkers for setting affinities

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     inputs.nixpkgs.follows = "nixpkgs";
   };
   inputs.sioworkers = {
-    url = "github:Stowarzyszenie-Talent/sioworkers/io-interactive-trial";
+    url = "github:Stowarzyszenie-Talent/sioworkers/affinity";
     inputs.nixpkgs.follows = "nixpkgs";
     inputs.filetracker.follows = "filetracker";
   };
